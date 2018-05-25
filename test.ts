f6877e3403282fbeb1f29f7153f2cf3cb0901eb1
Steven Enamakel <enamakel@cryptocontrol.io>
2018-05-25 16:41:37 +0530

    bump node

--- test.ts
+++ test.ts
@@ -1,8 +1,12 @@
-import CryptoControlApi from './index'
+import CryptoControlApi from './src/CryptoControlApi'
+import { IArticle } from './src/interfaces'
 
+
+// @ts-ignore
 const api = new CryptoControlApi(process.env.NEWS_API_KEY)
 
+
 api.getLatestNews()
-    .then(articles => {
+    .then((articles: IArticle) => {
         console.log(articles)
     })
